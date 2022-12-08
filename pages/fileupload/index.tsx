35fd1c936eda5ae2869ace12d009dcf6ec148b67
sejung1218 <sejung1218@hanmail.net>
2022-12-08 18:03:16 +0900

    file routing 경로 생성

---
+++ pages/fileupload/index.tsx
@@ -0,0 +1,8 @@
+import Router from "next/router";
+import * as React from "react";
+import { kakaoInit } from "../kakaoInit";
+import { Box, Button, Container, styled, Typography } from "@mui/material";
+
+export default function fileUpload() {
+  return <Box>TEST</Box>;
+}
